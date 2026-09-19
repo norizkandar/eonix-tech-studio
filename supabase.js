c17c1a213697abfe13a55df954934bf9a39d980f
MOHAMAD  NORIZKANDAR BIN MOHAMAD FAIZAL <norizkandarfaizal@gmail.com>
2026-09-19 16:00:45 +0800

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://efeayniqqqthzfkmwkia.supabase.co";
+const SUPABASE_KEY = "sb_publishable_sB4e5aWKf_dmlwmwJWDxjg_r2YBleoD";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
